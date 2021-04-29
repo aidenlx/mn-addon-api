fbd60710e7647ff01743e13f6e48f77ad33c81c6
AidenLx <aiden.lx@outlook.com>
2021-04-29 17:15:59 +0800

    1.2.0

--- out.d.ts
+++ out.d.ts
@@ -135,22 +135,73 @@ declare class JSExtension {
 }export {};
 
 declare global {
-  type NotifySender = {
-    [k: string]: any;
-    userInfo: {
-      [k: string]: any;
-      note?: MbBookNote;
-      documentController?: DocumentController;
-    };
+
+  type EventHandler<T extends NotifySender = NotifySender> = (
+    sender: T
+  ) => void;
+
+  type Event = NotifySender["name"];
+
+  type NotifySender =
+    | PopupMenuOnNote_Sender
+    | PopupMenuOnSelection_Sender
+    | ChangeExcerptRange_Sender
+    | ProcessNewExcerpt_Sender;
+
+  type Sender_Basic = {
+    name: Event;
+    object: UIView;
+    userInfo: { [k: string]: any };
+
+    performSelector(): void;
+    performSelectorWithObject(): void;
+    performSelectorWithObjectWithObject(): void;
+    doesNotRecognizeSelector(): void;
+    forwardingTargetForSelector(): void;
+    methodSignatureForSelector(): void;
+    retain(): void;
+    retainCount(): void;
+    autorelease(): void;
+    copy(): void;
+    finalize(): void;
+    dealloc(): void;
+    isEqual(): void;
+    self(): void;
+    isProxy(): void;
+    conformsToProtocol(): void;
+    hash(): void;
+    description(): void;
+    debugDescription(): void;
+    retainWeakReference(): void;
+    allowsWeakReference(): void;
+    forwardInvocation(): void;
+    mutableCopy(): void;
   };
 
-  type EventHandler = (sender: NotifySender) => void;
-
-  type Events =
-    | "PopupMenuOnNote"
-    | "PopupMenuOnSelection"
-    | "ChangeExcerptRange"
-    | "ProcessNewExcerpt";
+  interface PopupMenuOnNote_Sender extends Sender_Basic {
+    name: "PopupMenuOnNote";
+    userInfo: {
+      note: MbBookNote;
+    };
+  }
+  interface PopupMenuOnSelection_Sender extends Sender_Basic {
+    name: "PopupMenuOnSelection";
+    userInfo: {
+      documentController: DocumentController;
+    };
+  }
+  interface ChangeExcerptRange_Sender extends Sender_Basic {
+    name: "ChangeExcerptRange";
+    userInfo: {
+      noteid: string;
+    };
+  }
+  interface ProcessNewExcerpt_Sender extends Sender_Basic {
+    name: "ProcessNewExcerpt";
+    userInfo: {
+      noteid: string;
+    };
+  }
 }
 export {};
 
